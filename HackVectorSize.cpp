4bc9c6054a2011b0b322b85a92d7b0b7de65fe52
Skashyap <Shwetha.kashyap23@gmail.com>
2026-04-16 21:01:27 -0700

    Create HackVectorSize.cpp

---
+++ HackVectorSize.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct VectorProxy {
+    int* begin;
+    int* end;
+    int* cap_end;
+};
+
+/*
+Example of how to hacky way change the contents of a vector to simulate the memory overrun condition
+
+*/
+int main() {
+    vector<int> testVector(10, 9);
+    cout << &testVector << endl; 
+    VectorProxy* ptr = (VectorProxy*)(&testVector);
+    cout << ptr->begin << " and " << ptr->end << endl;
+    cout << *(ptr->begin) << " and " << *(ptr->end) << endl;
+    *(ptr->end) = 4112;
+    cout << *(ptr->begin) << " and " << *(ptr->end) << endl;
+}
